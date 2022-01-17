9cbd7b85e002f1d3c63febf8572d4cc432accda6
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-17 14:48:04 +0100

    milestone 1 done

--- myscript.js
+++ myscript.js
@@ -1,9 +1,35 @@
 let app = new Vue ({
     el: '#miapp',
     data: {
+        inputUtente: "",
+        listatodo : [
+            {
+               text: "Spesa",
+               done: false, 
+            },
+            {
+                text: "Farmacia",
+                done: false, 
+            },
+            {
+                text: "Lezione",
+                done: true, 
+            },
+
+            
+
+        ]
 
     },
     methods: {
 
+        aggiungiMansione : function(inputUtente){
+
+            nuovoOggetto = {
+                text: inputUtente,
+                done: false,            
+            };
+            this.listatodo.push(nuovoOggetto);
+        }
     }
 });
